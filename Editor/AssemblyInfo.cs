a7ad2817cb453114d03a1a5fcd582e745dd10d5a
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-23 23:33:52 +0100

    Split Avatar Tools into standalone package

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NekoSune.Optimizer.Editor")]
+[assembly: InternalsVisibleTo("NekoSune.Doctors.Editor")]
+[assembly: InternalsVisibleTo("NekoSune.Converters.Editor")]
