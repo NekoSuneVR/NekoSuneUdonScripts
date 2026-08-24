1755a02c6226251b6d515dc761f6415970f2a8ec
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-24 03:09:28 +0100

    Allow Animation Tools authoring assembly to access World Hub internals

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -10,6 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("NekoSune.WorldAvatarSearch.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.WorldPlayerTools.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.AnimationTools.Editor")]
+[assembly: InternalsVisibleTo("NekoSune.AnimationTools.Authoring.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Optimizer.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Doctors.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Converters.Editor")]
