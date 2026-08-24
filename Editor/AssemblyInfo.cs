ecffa9de978d0e776c44b127f0fbd822ae5dd61f
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-24 02:18:52 +0100

    Expose shared Avatar Tools API to Animation Tools

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("NekoSune.AnimationTools.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Optimizer.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Doctors.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Converters.Editor")]
