73b5e23087ea4f8a06f925dfb7421d33674a0468
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-24 02:18:46 +0100

    Add invariant culture shim for reflection value conversion

---
+++ Editor/AvatarSetup/NekoCultureInfoShim.cs
@@ -0,0 +1,7 @@
+namespace NekoSune.Avatars.Editor
+{
+    internal static class CultureInfo
+    {
+        public static System.IFormatProvider InvariantCulture { get { return System.Globalization.CultureInfo.InvariantCulture; } }
+    }
+}
