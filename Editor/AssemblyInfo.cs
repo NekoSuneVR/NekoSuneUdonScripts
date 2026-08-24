3804d437badd262b8b467bb759195781340aa8ca
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-24 02:15:08 +0100

    Set Animation Tools assembly sharing

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NekoSune.Optimizer.Editor")]
-[assembly: InternalsVisibleTo("NekoSune.Doctors.Editor")]
-[assembly: InternalsVisibleTo("NekoSune.Converters.Editor")]
