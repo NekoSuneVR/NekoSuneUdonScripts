f866e919e5e2c67d26d6b0ffede857540187395d
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-24 02:14:13 +0100

    Allow Animation Tools to register with Avatar Hub

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NekoSune.AvatarTools.Editor")]
+[assembly: InternalsVisibleTo("NekoSune.AnimationTools.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Optimizer.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Doctors.Editor")]
 [assembly: InternalsVisibleTo("NekoSune.Converters.Editor")]
